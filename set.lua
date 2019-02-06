20d08efc9c0caa450ae42088cec264b4d9ea3151
Jason Perkins <starkos@industriousone.com>
2019-02-06 18:01:56 -0500

    Implement initial APIs

--- set.lua
+++ set.lua
@@ -1,10 +1,153 @@
 ---
 -- set/set.lua
 --
+-- Sets behave like a hybrid array and table. Values may be indexed like an array
+-- and are returned in the order in which they are added. Each value may only
+-- occur once; duplicate values are discarded and only the first is kept.
+--
 -- Author Jason Perkins
 -- Copyright (c) 2019 Jason Perkins and the Premake project
 ---
 
-local m = {}
+	local m = {}
 
-return m
+
+---
+-- Construct a new Set object.
+---
+
+	function m.new(...)
+		local self = {}
+
+		local n = select('#', ...)
+		for i = 1, n do
+			m.add(self, select(i, ...))
+		end
+
+		return self
+	end
+
+
+---
+-- Add a new value to the set. If the value is already in the set, it is not
+-- added again.
+---
+
+	function m.add(self, value)
+		if self[value] == nil then
+			local index = #self + 1
+			self[index] = value  -- store in an array to maintain ordering
+			self[value] = index  -- store in map for quick lookups
+		end
+		return self
+	end
+
+
+---
+-- Does the specified item exist in the set?
+---
+
+	function m.contains(self, value)
+		return (self[value] ~= nil)
+	end
+
+
+---
+-- Iterator, enumerates all items in order
+---
+
+	function m.each(self)
+		local i = 0
+
+		return function ()
+			i = i + 1
+			if i <= #self then
+				return self[i]
+			end
+		end
+	end
+
+
+---
+-- Call the provided function for each item in the set, in order
+---
+
+	function m.forEach(self, func)
+		local n = #self
+		for i = 1, n do
+			func(self[i])
+		end
+	end
+
+
+---
+-- Is the set empty?
+---
+
+	function m.isEmpty(self)
+		return (#self == 0)
+	end
+
+
+---
+-- Call the provided function for each item in the set, in order, and replace
+-- the item with the return value of the function. If a `nil` is returned,
+-- the item is removed from the set.
+---
+
+	function m.map(self, func)
+		local i = 1
+		while i <= #self do
+			local newValue = func(self[i])
+			if newValue ~= nil then
+				self[i] = newValue
+				i = i + 1
+			else
+				m.remove(self, self[i])
+			end
+		end
+	end
+
+
+---
+-- Remove a value from the set.
+---
+
+	function m.remove(self, value)
+		local index = self[value]
+		if index ~= nil then
+			table.remove(self, index)
+			self[value] = nil
+		end
+	end
+
+
+---
+-- Remove the value at the given array index from the set.
+---
+
+	function m.removeAt(self, index)
+		local value = self[index]
+		table.remove(self, index)
+		self[value] = nil
+	end
+
+
+---
+-- Add all items from a second set that aren't present in this one.
+---
+
+	function m.union(self, other)
+		local n = #other
+		for i = 1, n do
+			local value = other[i]
+			m.add(self, value)
+		end
+	end
+
+
+---
+-- End of module
+---
+
+	return m
--- tests/test_set.lua
+++ tests/test_set.lua
@@ -6,21 +6,205 @@
 ---
 
 	local suite = test.declare('set')
-	local set = require('set')
+
+	local Set = require('set')
 
 
 ---
--- Setup and teardown
+-- Can construct a new Set object
 ---
 
-	function suite.setup()
+	function suite.new_returnsNewSet()
+		local s = Set.new()
+		test.isnotnil(s)
 	end
 
 
 ---
--- Set up a dummy test just to get things started
+-- Can populate set with constructor arguments
 ---
 
-	function suite.firstTest()
-		test.isnil(nil)
+	function suite.new_populatesArray()
+		local s = Set.new('A', 'B', 'C')
+		test.isequal(3, #s)
+		test.isequal('A', s[1])
+		test.isequal('B', s[2])
+		test.isequal('C', s[3])
+	end
+
+
+---
+-- Can add a value and fetch it back via array indexing
+---
+
+	function suite.add_addsItemToArray()
+		local s = Set.new()
+		Set.add(s, 'value')
+
+		test.isequal(1, #s)
+		test.isequal('value', s[1])
+	end
+
+
+---
+-- Add should not allow duplicate values
+---
+
+	function suite.add_preventsDuplicates()
+		local s = Set.new()
+		Set.add(s, 'value')
+		Set.add(s, 'value')
+
+		test.isequal(1, #s)
+		test.isequal('value', s[1])
+	end
+
+
+---
+-- Add should allow unique values
+---
+
+	function suite.add_allowsUnique()
+		local s = Set.new()
+		Set.add(s, 'value1')
+		Set.add(s, 'value2')
+
+		test.isequal(2, #s)
+		test.isequal('value1', s[1])
+		test.isequal('value2', s[2])
+	end
+
+
+---
+-- `contains` should return true if the item is in the set, and false when not
+---
+
+	function suite.contains_returnsTrueIfPresent()
+		local s = Set.new('A', 'B', 'C')
+		test.istrue(Set.contains(s, 'B'))
+	end
+
+	function suite.contains_returnsFalseIfNotPresent()
+		local s = Set.new('A', 'B', 'C')
+		test.isfalse(Set.contains(s, 'X'))
+	end
+
+
+---
+-- `each` enumerates each item in the order it was added
+---
+
+	function suite.each_enumeratesAllItems()
+		local result = ''
+
+		local s = Set.new('A', 'B', 'C')
+		for item in Set.each(s) do
+			result = result .. item
+		end
+
+		test.isequal('ABC', result)
+	end
+
+
+---
+-- `forEach` calls a function for each item in the order it was added
+---
+
+	function suite.forEach_enumeratesAllItems()
+		local result = ''
+
+		local s = Set.new('A', 'B', 'C')
+		Set.forEach(s, function(item)
+			result = result .. item
+		end)
+
+		test.isequal('ABC', result)
+	end
+
+
+---
+-- `isEmpty` should return true when empty and false when not
+---
+
+	function suite.isEmpty_returnsTrueIfEmpty()
+		local s = Set.new()
+		test.istrue(Set.isEmpty(s))
+	end
+
+	function suite.isEmpty_returnsFalseIfNotEmpty()
+		local s = Set.new('A', 'B', 'C')
+		test.isfalse(Set.isEmpty(s))
+	end
+
+
+---
+-- `map` should call a function for each item, in order, and
+-- replace the item with the return value of the function
+---
+
+	function suite.map_replacesEachValueWithReturn()
+		local s = Set.new('A', 'B', 'C')
+		Set.map(s, function(item) return string.lower(item) end)
+
+		test.isequal('a', s[1])
+		test.isequal('b', s[2])
+		test.isequal('c', s[3])
+	end
+
+	function suite.map_removesItem_onNilReturn()
+		local s = Set.new('A', 'B', 'C')
+		Set.map(s, function(item)
+			if item ~= 'B' then
+				return string.lower(item)
+			end
+		end)
+
+		test.isequal(2, #s)
+		test.isequal('a', s[1])
+		test.isequal('c', s[2])
+	end
+
+
+---
+-- `remove` should remove the item from the array
+---
+
+	function suite.remove_removesItem()
+		local s = Set.new('A', 'B', 'C')
+		Set.remove(s, 'B')
+
+		test.isequal(2, #s)
+		test.isequal('A', s[1])
+		test.isequal('C', s[2])
+	end
+
+
+---
+-- `removeAt` should remove the item at the given array index
+---
+
+	function suite.removeAt_removesItemAtIndex()
+		local s = Set.new('A', 'B', 'C')
+		Set.removeAt(s, 2)
+
+		test.isequal(2, #s)
+		test.isequal('A', s[1])
+		test.isequal('C', s[2])
+	end
+
+
+---
+-- `union` should add items that aren't already present
+---
+
+	function suite.union_addsMissingItems()
+		local s = Set.new('A', 'B', 'C')
+		local t = Set.new('B', 'C', 'D')
+		Set.union(s, t)
+
+		test.isequal(4, #s)
+		test.isequal('A', s[1])
+		test.isequal('B', s[2])
+		test.isequal('C', s[3])
+		test.isequal('D', s[4])
 	end
